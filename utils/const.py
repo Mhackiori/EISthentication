b14b60772a4dd59c16b50160b7adf4c11217b745
Francesco Marchiori <francesco.marchiori98@gmail.com>
2023-04-26 18:55:36 +0200

    Added plots

--- utils/const.py
+++ utils/const.py
@@ -8,7 +8,16 @@ RAW = './Datasets/Raw/'
 PROCESSED = './Datasets/Processed/'
 RESULTS = './Results/'
 FIGURES = './Results/Figures/'
+COMPLEXITY = './Results/Complexity/'
 
 # Datasets
 if os.path.exists(PROCESSED):
     DATASETS = sorted(os.listdir(PROCESSED))
+
+# Balance
+BALANCES = [
+    [50, 50],
+    [60, 40],
+    [70, 30],
+    [80, 20]
+]
